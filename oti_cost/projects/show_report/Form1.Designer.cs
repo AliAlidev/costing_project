469824a8ff6da21f910bf0e4987d949987a11f88
ali ali <ali.ali.deve@gmail.com>
2022-03-29 11:03:03 +0300

    fix

--- oti_cost/projects/show_report/Form1.Designer.cs
+++ oti_cost/projects/show_report/Form1.Designer.cs
@@ -46,7 +46,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
             this.Controls.Add(this.reportViewer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
